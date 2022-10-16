9a9e9dcc9cae363e80fd0b43e79fd11098745f79
root <root@windows-pedro.localdomain>
2022-10-16 12:05:15 -0600

    aqui va la primer version de roi

--- funciones/comerciales.py
+++ funciones/comerciales.py
@@ -1,5 +1,12 @@
 def precio_descuento():
     print('estamos en la funcion precio_descuento')
 
+def roi(costo, ingreso):
+    return (ingreso - costo)/ costo
+
 ## aqui llamamos a la funcion
 precio_descuento()
+print('El roi es:' + str(roi(20.00,100.00)))
+
+
+
